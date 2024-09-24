6f9d4ee72c2de5b863ba3719ded89ea4b67f670f
Chaela <chaelabird@gmail.com>
2024-09-24 14:00:02 -0700

    Updated PLAN file, modified PlayerMovement.

--- Egg Hunt/Assets/Scripts/PlayerMovement.cs
+++ Egg Hunt/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField]
+   
     float MoveSpeed = 10f;
+    [SerializeField]
+    float Sprint = 10f;
+    [SerializeField]
+    float MoveSpeedDef = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +21,22 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetButtonDown("left shift"))
+        {
+            MoveSpeed += Sprint;
+        }
+        else { }
         //check x y input
         //move play based on input
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
         //velocity is vector2 varible, storing 2 floats, x and y.
         GetComponent<Rigidbody2D>().velocity = new Vector2(xInput, yInput) * MoveSpeed;
-      
+        if (Input.GetButtonUp("left shift"))
+        {
+            MoveSpeed = MoveSpeedDef;
+        }
+        else { }
     }
 }
